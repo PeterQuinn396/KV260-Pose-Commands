3783935f6e9a3f50fc09a5276ca8f095bba53a5e
Peter Quinn <peter.quinn396@gmail.com>
2022-01-03 14:24:09 -0500

    Prepare quantize script

--- model/model.py
+++ model/model.py
@@ -17,26 +17,66 @@ class GestureClassifyModel(torch.nn.Module):
         self.model.eval()
 
     def forward(self, input):
+        # input is [BATCH_SIZE, 3, 224, 244]
         return self.model.forward(input)
 
 
-def loss_fn(v):
-    return torch.nn.CrossEntropyLoss(v)
+def loss_fn(out, labels):
+    return torch.nn.CrossEntropyLoss(out, labels)
 
 
-def get_dataloader():
-    dataset = ASLDataset("/gdrive/My Drive/Projects/kv260/asl_dataset", transform=preprocess)
+def get_dataloader(data_set_path: str):
+    dataset = ASLDataset(data_set_path, transform=preprocess)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True)
     return dataloader
 
 
-def test_vitis_compatible():
-    pass
+def test_vitis_compatible(model):
+    """
+    Run the torch jit check to make sure the model is quatizable
+    by Vitis
+    :param model:
+    :return:
+    """
+    test_input = torch.rand((1,3,224,224)).to(device)
+    try:
+        tr_func = torch.jit.trace(model, test_input)
+    except:
+        return False
+    return True
 
 
 def train():
     pass
 
 
-def test():
-    pass
+def test(model: torch.nn.Module, test_loader: torch.utils.data.DataLoader, max_samples=100):
+    model.eval()
+
+    running_corrects = 0
+    running_loss = 0
+
+    count = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            loss = loss_fn(outputs, labels)
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+            running_corrects += torch.sum(preds == torch.argmax(labels, 1))
+
+            # put a max on the number of samples tested
+            count += len(inputs)
+            if count > max_samples:
+                break
+
+    epoch_loss = running_loss / count
+    epoch_acc = running_corrects.double() / count
+
+    return epoch_acc, epoch_loss
+
--- model/quantize.py
+++ model/quantize.py
@@ -1,9 +1,48 @@
-
 import torch
+from ..common import device
 from .asl_dataset import ASLDataset, preprocess
-from .model import loss_fn, GestureClassifyModel
+from .model import loss_fn, GestureClassifyModel, get_dataloader, test
+import argparse
 
+from pytorch_nndct.apis import torch_quantizer, dump_xmodel
+
+"""
+To be run inside the Vitis AI docker, after running `conda activate vitis-ai-pytorch`
+"""
+
+def quantize(model, val_loader, quant_mode):
+    input = torch.randn([1, 3, 224, 224])
+    quantizer = torch_quantizer(quant_mode, model, (input))
+    quant_model = quantizer.quant_model
+
+    # make custom test fn, that takes a torch data loader and a loss_fn
+    acc1_gen, loss_gen = test(quant_model, val_loader)
+    print(f'Acc: {acc1_gen}, loss: {loss_gen}')
+
+    if quant_mode == 'calib':
+        quantizer.export_quant_config()
+    if quant_mode == 'test':
+        quantizer.export_xmodel(deploy_check=False)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--quant_mode', choices=['test', 'calib'])
+    parser.add_argument('--deploy', action='store_true')
+    parser.add_argument('--subset_length', default=1)
+    return parser
 
 
 def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+
     model = GestureClassifyModel("asl_resnext.pth")
+    model.to(device)
+    dataloader = get_dataloader('dataset/original_frames')
+
+    quantize(model, dataloader, args.quant_mode)
+
+if __name__ == "__main__":
+    main()
